a44a188697fc1740e04289cc607f0db6ce5dba58
thehansguy <thehansguy@gmail.com>
2022-01-25 19:16:54 +0000

    Changes to be committed: 	modified:   index.html 	modified:   script.js
    // data bindings

---
+++ script.js
@@ -0,0 +1,14 @@
+// data bindings
+const previousOperandButtons = document.querySelectorAll(
+  "[data-previous-operand]"
+);
+const currentOperandButtons = document.querySelectorAll(
+  "[data-current-operand]"
+);
+const allClearButtons = document.querySelectorAll("[data-all-clear]");
+const deleteButtons = document.querySelectorAll("[data-delete]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const numberButtons = document.querySelectorAll("[data-number]");
+const equalsButtons = document.querySelectorAll("[data-equals]");
+
+// functionality functions
